33edb5e33dce33325427f27c0135840435b3c612
fil7 <filonova7@yandex.ru>
2016-11-30 13:32:40 +0700

    hw0: implementation of UserMealsUtil.getFilteredWithExceeded

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,5 +1,9 @@
 package ru.javawebinar.topjava;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
 /**
  * User: gkislin
  * Date: 05.08.2015
@@ -9,6 +13,11 @@ package ru.javawebinar.topjava;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.format("Hello Topjava Enterprise!");
+        //System.out.format("Hello Topjava Enterprise!");
+
+        LocalDate localDate = LocalDate.now();
+        localDate = localDate.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+
+
     }
 }
