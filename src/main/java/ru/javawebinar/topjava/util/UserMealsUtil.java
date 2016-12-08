9527ed3b234f198e1dde36c5ea07887d36dcd348
fil7 <filonova7@yandex.ru>
2016-12-08 12:13:21 +0700

    hw0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -24,6 +24,8 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
         List<UserMealWithExceed> userMealWithExceeds = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        userMealWithExceeds.forEach(meal -> System.out.println(meal));
+//        .toLocalDate();
 //        .toLocalDate();
 //        .toLocalTime();
     }
